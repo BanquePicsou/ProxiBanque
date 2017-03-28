560acd1a1f6ff7570918f73abe8c010cf9b1cbec
inti0302 <inti0302@DESKTOP-9TPILNO.home>
2017-03-28 17:10:11 +0200

    correction dao personne

--- 01_Project/src/main/java/fr/adaming/dao/PersonneDaoImpl.java
+++ 01_Project/src/main/java/fr/adaming/dao/PersonneDaoImpl.java
@@ -3,46 +3,81 @@ package fr.adaming.dao;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.Query;
 
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 
 import fr.adaming.entities.Personne;
 
-@Component
-@Transactional
+/**
+ * Classe générique Dao pour les personnes(Client, Conseiler, Admin et Gérant)
+ * Communique avec la BDD
+ * @author inti0302
+ *
+ * @param <T>
+ */
+@Repository
 public class PersonneDaoImpl<T extends Personne> implements IPersonneDao<T> {
 
-	@PersistenceContext
-	EntityManager em;
+	@Autowired
+	EntityManagerFactory emf;
 						
-	@Override
-	public void addPersonne(T personne) {
-		em.persist(personne);		
+	/**
+	 * Le setter de l'EMF pour le fonctionnement correct de l'autowired
+	 * @return
+	 */
+	public void setEmf(EntityManagerFactory emf) {
+		this.emf = emf;
 	}
 
+	/**
+	 * Methode generique pour ajouter une personne
+	 */
+	@Override
+	public void addPersonne(T personne) {
+		EntityManager em = emf.createEntityManager();
+		em.persist(personne);
+	}
+
+	/**
+	 * Methode generique pour delete une personne
+	 */
 	@Override
 	public void deletePersonne(T personne) {
+		EntityManager em = emf.createEntityManager();
 		em.remove(personne);	
 	}
 
+	/**
+	 * Methode generique pour recupérer la liste des personnes
+	 */
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<Personne> getList() {
+		EntityManager em = emf.createEntityManager();
 		String req = "Select p FROM Personne p";
 		Query query = em.createQuery(req);
 		return query.getResultList();
 	}
 
+	/**
+	 * Methode generique pour récupérer une personne
+	 */
 	@Override
 	public Personne getPersonne(int id) {
+		EntityManager em = emf.createEntityManager();
 		return em.find(Personne.class, id);
 	}
 
+	/**
+	 * Methode générique pour mettre à jour une personne
+	 */
 	@Override
 	public void updatePersonne(T personne) {
+		EntityManager em = emf.createEntityManager();
 		em.merge(personne);		
 	}
 
