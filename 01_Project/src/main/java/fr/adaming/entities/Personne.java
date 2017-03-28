b9db98898c1e408cefd0458fbc4aa0218ffe167d
inti0302 <inti0302@DESKTOP-9TPILNO.home>
2017-03-28 15:18:50 +0200

    fichier properties

--- 01_Project/src/main/java/fr/adaming/entities/Personne.java
+++ 01_Project/src/main/java/fr/adaming/entities/Personne.java
@@ -1,5 +1,26 @@
 package fr.adaming.entities;
 
-public class Personne {
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 
+/**
+ * La classe abstraite Personne comprend les informations de bases 
+ * lié à toute persone liée 
+ * @author inti0302
+ *
+ */
+@Entity
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="rôle")
+public abstract class Personne {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
 }
