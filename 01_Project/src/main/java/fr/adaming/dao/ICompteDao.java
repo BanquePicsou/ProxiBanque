fd764377e97460440b02460cadb4f677d265d824
inti0242 <inti0242@DESKTOP-0QCO84F.home>
2017-03-28 17:17:18 +0200

    juju comptedao

---
+++ 01_Project/src/main/java/fr/adaming/dao/ICompteDao.java
@@ -0,0 +1,16 @@
+package fr.adaming.dao;
+
+import java.util.List;
+
+import fr.adaming.entities.Compte;
+
+public interface ICompteDao<T extends Compte> {
+
+	
+	public void addCompte(T compte);
+	public void deleteCompte(T compte);
+	public List<Compte> getList();
+	public Compte getCompte(int id);
+	public void updateCompte(T compte);
+	
+}
