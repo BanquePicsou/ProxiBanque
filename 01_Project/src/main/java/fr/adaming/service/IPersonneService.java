5b9a62544e4f483411ec771227a2c37c6bb27038
inti0302 <inti0302@DESKTOP-9TPILNO.home>
2017-03-29 10:06:36 +0200

    Robin : Personne Service Ok (manque associations) + JAVADOC

--- 01_Project/src/main/java/fr/adaming/service/IPersonneService.java
+++ 01_Project/src/main/java/fr/adaming/service/IPersonneService.java
@@ -7,22 +7,39 @@ import fr.adaming.entities.Conseiller;
 import fr.adaming.entities.Gerant;
 import fr.adaming.entities.Personne;
 
+/**
+ * Interface du service Personne.
+ * Le service assure la communication entre le controller et la DAO.
+ * @author Robin
+ *
+ */
 public interface IPersonneService {
 	
 	/* Les methodes pour ajouter une personne */
+	/** Ajoute un client en appelant la méthode générique Dao ajout Personne */
 	public void addClient (Client client);
+	/** Ajoute un gérant en appelant la méthode générique Dao ajout Personne */
 	public void addGerant (Gerant gerant);
+	/** Ajoute un conseiller en appelant la méthode générique Dao ajout Personne */
 	public void addConseiller (Conseiller conseiller);
 	
 	/* pour supprimer */
+	/** Cette methode peut supprimer n'importe quelle personne selon son id */
 	public void deletePersonne (int id);
 	
 	/* pour récupérer */
+	/** Permet de récupérer n'importe quelle personne selon son ID */
 	public Personne getPersonne (int id);
+	/** Récupère la liste intégrale des personnes et ses classes filles */
 	public List<Personne> getAllPersonne();
+	/** Permet de récupérer la liste des clients à l'aide des régles de gestion définie dans la classe service */
 	public List<Client> getAllClients();
+	/** Permet de récupérer la liste des gérants à l'aide des régles de gestion définie dans la classe service */
 	public List<Gerant> getAllGerant();
+	/** Permet de récupérer la liste des conseiler à l'aide des régles de gestion définie dans la classe service */
 	public List<Conseiller> getAllConseiller();
+	/** Permet de récupérer la liste des conseiller pour un gérant à l'aide des régles de gestion définie dans la classe service */
 	public List<Conseiller> getConseillersByGerant (Gerant gerant);
+	/** Permet de récupérer la liste des clients par conseiler à l'aide des régles de gestion définie dans la classe service */
 	public List<Client> getClientsByConseiller (Conseiller conseiller);
 }
--- 01_Project/src/main/java/fr/adaming/service/PersonneServiceImpl.java
+++ 01_Project/src/main/java/fr/adaming/service/PersonneServiceImpl.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import fr.adaming.dao.IPersonneDao;
 import fr.adaming.entities.Client;
@@ -11,14 +13,38 @@ import fr.adaming.entities.Conseiller;
 import fr.adaming.entities.Gerant;
 import fr.adaming.entities.Personne;
 
+/**
+ * La classe PersonneServiceImpl implemente 
+ * l'interface IPersonneService et redefini ses méthodes
+ * C'est ici que sont définie les differentes regles de gestion et
+ * l'appel de la classe  + interface DAO pour l'interaction avec
+ * la base de donnée.
+ * @Service : reconnaissance en tant que Service par Spring IoC
+ * @Transactional : le service va initier les transactions avec la BDD
+ * @author Robin
+ *
+ */
+@Service
+@Transactional
 public class PersonneServiceImpl implements IPersonneService {
 
+	/**
+	 * L'attribut personneDao est utilisé pour l'appel des 
+	 * méthodes de la classe PersoneDao. 
+	 * Il est annoté par @Autowired pour l'injection de dépendance
+	 * @SuppresWarnings retire l'avertissement lié à la généricité de la 
+	 * classe+interface PersonneDao
+	 */
 	@SuppressWarnings("rawtypes")
 	@Autowired
 	IPersonneDao personneDao;
 	
 	
-	
+	/** Ajoute un client en appelant la méthode générique Dao ajout Personne
+	 * Comme il s'agit d'un client, le rôle "client" lui est attribué avant 
+	 * la transmission à la base de donnée
+	 * @param client : le conseiller créer par l'utilisateur 
+	 * @SuprresWarning annule l'erreur lié à la généricité de PersonneDao */
 	@SuppressWarnings("unchecked")
 	@Override
 	public void addClient(Client client) {
@@ -27,6 +53,11 @@ public class PersonneServiceImpl implements IPersonneService {
 		
 	}
 
+	/** Ajoute un gerant en appelant la méthode générique Dao ajout Personne
+	 * Comme il s'agit d'un gerant, le rôle "gerant" lui est attribué avant 
+	 * la transmission à la base de donnée
+	 * @param gerant : le gerant créer par l'utilisateur 
+	 * @SuprresWarning annule l'erreur lié à la généricité de PersonneDao */
 	@SuppressWarnings("unchecked")
 	@Override
 	public void addGerant(Gerant gerant) {
@@ -34,6 +65,11 @@ public class PersonneServiceImpl implements IPersonneService {
 		personneDao.addPersonne(gerant);
 	}
 
+	/** Ajoute un conseiller en appelant la méthode générique Dao ajout Personne
+	 * Comme il s'agit d'un conseiller, le rôle "conseiller" lui est attribué avant 
+	 * la transmission à la base de donnée
+	 * @param conseiller : le conseiller créer par l'utilisateur 
+	 * @SuprresWarning annule l'erreur lié à la généricité de PersonneDao */
 	@SuppressWarnings("unchecked")
 	@Override
 	public void addConseiller(Conseiller conseiller) {
@@ -42,58 +78,147 @@ public class PersonneServiceImpl implements IPersonneService {
 		
 	}
 
+	/** Cette methode peut supprimer n'importe quelle personne selon son id
+	 * La personne est récupérée via la methode getByID puis envoyée à la 
+	 * DAO via une méthode générique.
+	 * @param id : identifiant dans la bdd de l'individu à supprimer
+	 * @SuprresWarning annule l'erreur lié à la généricité de PersonneDao  */
+	@SuppressWarnings("unchecked")
 	@Override
 	public void deletePersonne(int id) {
-		// TODO Auto-generated method stub
-		
+		personneDao.deletePersonne(personneDao.getPersonne(id));	
 	}
 
+
+	/**
+	 * Retourne un individu enregistré dans la bdd (client, conseiller, gerant ou admin) 
+	 * @param id : identifiant dans la bdd de l'individu recherché
+	 * @return Personne (comprenant ses classes filles) 
+	 */
 	@Override
-	public Personne getPersonne(int id) {
-		// TODO Auto-generated method stub
-		return null;
+	public Personne getPersonne(int id) {	
+		return personneDao.getPersonne(id);
 	}
 
+	/**
+	 * 
+	 * Cette méthode n'a pas de selection de type(client, conseiller, gerant ou admin)
+	 * @SuprresWarning annule l'erreur lié à la généricité de PersonneDao
+	 * @return liste intégrale des indivudes enregistrés dans la base 
+	 */
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<Personne> getAllPersonne() {
-		// TODO Auto-generated method stub
-		return null;
+		return personneDao.getList();
 	}
 
+	
+	/**
+	 * Permet de sortir la liste des clients de la base de donnée
+	 * @return listeRetour : la liste des clients, selectionés de la liste complète des personnes 
+	 * à partir du rôle "client"
+	 * @SuprresWarning annule l'erreur lié à la généricité de PersonneDao
+	 */
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<Client> getAllClients() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Gerant> getAllGerant() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Conseiller> getAllConseiller() {
-		@SuppressWarnings("unchecked")
 		List<Personne> liste = personneDao.getList();
-		List<Conseiller> listeRetour = new ArrayList<>();
+		List<Client> listeRetour = new ArrayList<>();
 		for(Personne p:liste){
-			if(p.getRole().equals("conseiller"));
-			listeRetour.add((Conseiller) p);
+			if(p.getRole().equals("client")){
+				listeRetour.add((Client) p);
+			}
 		}
 		return listeRetour;
 	}
 
+	
+	/**
+	 * Permet de sortir la liste des gerant de la base de donnée
+	 * @return listeRetour : la liste des gerant, selectionés de la liste complète des personnes 
+	 * à partir du rôle "gerant"
+	 * @SuprresWarning annule l'erreur lié à la généricité de PersonneDao
+	 */
+	@SuppressWarnings("unchecked")
 	@Override
-	public List<Conseiller> getConseillersByGerant(Gerant gerant) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Gerant> getAllGerant() {
+		List<Personne> liste = personneDao.getList();
+		List<Gerant> listeRetour = new ArrayList<>();
+		for(Personne p:liste){
+			if(p.getRole().equals("gerant")){
+				listeRetour.add((Gerant) p);
+			}
+		}
+		return listeRetour;
 	}
 
+	/**
+	 * Permet de sortir la liste des conseiller de la base de donnée
+	 * @return listeRetour : la liste des conseiller, selectionés de la liste complète des personnes 
+	 * à partir du rôle "conseiller"
+	 * @SuprresWarning annule l'erreur lié à la généricité de PersonneDao
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Conseiller> getAllConseiller() {	
+		List<Personne> liste = personneDao.getList();
+		List<Conseiller> listeRetour = new ArrayList<>();
+		for(Personne p:liste){
+			if(p.getRole().equals("conseiller")){
+			listeRetour.add((Conseiller) p);
+			}
+		}
+		return listeRetour;
+	}
+
+	/**
+	 * Permet de sortir la liste des conseiller d'un gérant
+	 * La liste générique des personnes est récupérée.
+	 * Ensuite, chaque conseiler est identifié via son rôle.
+	 * Si le gérant correspond, le conseiler est ajouté à la liste de retour
+	 * @param gerant : le gerant dont on veut la liste de conseiller
+	 * @return liste de conseiller trié selon le gérant
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Conseiller> getConseillersByGerant(Gerant gerant) {
+		List<Personne> liste = personneDao.getList();
+		List<Conseiller> listeRetour = new ArrayList<>();
+		for(Personne p:liste){
+			if(p.getRole().equals("conseiller")){
+				/* Cast en conseiller pour utiliser les méthodes de conseiller */
+				Conseiller c = (Conseiller) p;
+				if(c.getGerant().equals(gerant)){
+					listeRetour.add(c);
+				}
+			}
+		}
+		return listeRetour;
+	}
+
+	/**
+	 * Permet de sortir la liste des client d'un conseilelr
+	 * La liste générique des personnes est récupérée.
+	 * Ensuite, chaque client est identifié via son rôle.
+	 * Si le conseiller correspond, le client est ajouté à la liste de retour
+	 * @param conseiller : le conseiller dont on veut la liste de client
+	 * @return liste de client trié selon le conseiller
+	 */
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<Client> getClientsByConseiller(Conseiller conseiller) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Personne> liste = personneDao.getList();
+		List<Client> listeRetour = new ArrayList<>();
+		for(Personne p:liste){
+			if(p.getRole().equals("client")){
+				/* Cast en client pour utiliser les méthodes de conseiller */
+				Client c = (Client) p;
+				if(c.getConseiller().equals(conseiller)){
+					listeRetour.add(c);
+				}
+			}
+		}
+		return listeRetour;
 	}
 
 }
