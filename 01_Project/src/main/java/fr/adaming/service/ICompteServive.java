d13fa3ee6ae37058c57f211c36453dcc45bbd0af
inti0242 <inti0242@DESKTOP-0QCO84F.home>
2017-03-29 09:20:24 +0200

    juju service compte

---
+++ 01_Project/src/main/java/fr/adaming/service/ICompteServive.java
@@ -0,0 +1,28 @@
+package fr.adaming.service;
+
+import java.util.List;
+
+import fr.adaming.entities.Compte;
+import fr.adaming.entities.CompteCourant;
+import fr.adaming.entities.CompteEpargne;
+
+
+public interface ICompteServive {
+
+	/* Les methodes pour ajouter un compte */
+	public void addCompteEpargne (CompteEpargne ce);
+	public void addCompteCourant (CompteCourant cc);
+	
+	
+	/* pour supprimer */
+	public void deleteCompte (int id);
+	
+	/* pour récupérer un compte*/
+	public Compte getCompte (int id);
+	
+	/*recupere liste*/
+	public List<CompteEpargne> getAllCompteEpargne();
+	public List<CompteCourant> getAllCompteCourant();
+
+	
+}
