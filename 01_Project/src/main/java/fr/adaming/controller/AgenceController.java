1493f2c4dc513207cd90468c3b721177a838b603
inti0302 <inti0302@DESKTOP-9TPILNO.home>
2017-03-30 15:40:59 +0200

    virage de com'

--- 01_Project/src/main/java/fr/adaming/controller/AgenceController.java
+++ 01_Project/src/main/java/fr/adaming/controller/AgenceController.java
@@ -1,8 +1,5 @@
 package fr.adaming.controller;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-
-import java.io.Serializable;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +11,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import fr.adaming.entities.Agence;
-import fr.adaming.entities.Client;
-import fr.adaming.entities.Conseiller;
 import fr.adaming.service.IAgenceService;
 
 @Controller
@@ -35,16 +30,11 @@ public class AgenceController{
 	
 	@RequestMapping(value="/ajoutAgence", method=RequestMethod.GET)
 	public ModelAndView afficherFormulaireAgence(){
-		System.out.println("--debuggage : on rentre dans la methode qui charge le formulaire de création d'agence: \n");
 		return new ModelAndView("BigBoss/newAgence","command",new Agence());
 	}
 	
 	@RequestMapping(value="/soumettreAjouterAgence", method=RequestMethod.POST)
 	public String enregistrementAgence(ModelMap modelMap, Agence pAgence){
-		System.out.println("--debuggage: on rentre dans la methode qui envois le formulaire Agence au service : \n");
-		System.out.println("L'agence est :");
-		System.out.println(pAgence);
-		System.out.println("--------------------------");
 		agenceService.addAgence(pAgence);
 		List<Agence> listeAgence = agenceService.getList();
 		modelMap.addAttribute("agenceListe", listeAgence);
@@ -53,8 +43,6 @@ public class AgenceController{
 	
 	@RequestMapping(value="/deleteAgence/{idA}", method=RequestMethod.GET)
 	public String deleteAgence(ModelMap modelMap,@PathVariable("idA") int id){
-		System.out.println("--debuggage: on rentre dans la methode qui supprime une agence \n id à supprimer :" + id);
-		System.out.println("Agence à supprimer"+agenceService.getAgence(id));
 		agenceService.deleteAgence(agenceService.getAgence(id));
 		List<Agence> listeAgence = agenceService.getList();
 		modelMap.addAttribute("agenceListe", listeAgence);
@@ -63,7 +51,6 @@ public class AgenceController{
 	
 	@RequestMapping(value="/updateAgence/{idA}", method=RequestMethod.GET)
 	public String updateVueAgence(ModelMap modelMap,@PathVariable("idA") int id){
-		System.out.println("--debuggage: on rentre dans la methode qui modifieune agence \n");
 		Agence agenceAModifier=agenceService.getAgence(id);
 		modelMap.addAttribute("AgenceAModifier", agenceAModifier);
 		return "BigBoss/ModifAgence"; 
@@ -71,11 +58,6 @@ public class AgenceController{
 	
 	@RequestMapping(value="/soumettreUpdateAgence", method=RequestMethod.POST)
 	public String updateSoumettreAgence(ModelMap modelMap, Agence pAgence){
-		System.out.println("--debuggage: on rentre dans la methode qui envois le formulaire Agence au service : \n");
-		System.out.println("L'agence est :");
-		System.out.println(pAgence);
-		System.out.println("--------------------------");
-		agenceService.updateAgence(pAgence);
 		List<Agence> listeAgence = agenceService.getList();
 		modelMap.addAttribute("agenceListe", listeAgence);
 		return "BigBoss/listeAgence";
