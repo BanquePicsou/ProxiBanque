5cc4c4b1495e68553e822a8ecd9c442172bbc467
inti0302 <inti0302@DESKTOP-9TPILNO.home>
2017-03-30 11:27:15 +0200

    List Controller ok

---
+++ 01_Project/src/main/java/fr/adaming/controller/ConseillerController.java
@@ -0,0 +1,45 @@
+package fr.adaming.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import fr.adaming.entities.Client;
+import fr.adaming.service.IPersonneService;
+
+/**
+ * Controller pour des methoders spécifiques d'un conseiller :
+ * 1)Afficher une liste de client
+ * 2)Afficher sa liste de client 
+ * 3)Virement de compte à  compte
+ * 
+ * @author Robin
+ *
+ */
+@Controller
+@RequestMapping("/user/conseiller")
+public class ConseillerController {
+
+	/**
+	 * Attribut utilisé pour l'appel du service
+	 * @Autowired pour l'injection de dépendance
+	 */
+	@Autowired
+	IPersonneService personneService;
+	
+	@RequestMapping(value="/allclient", method=RequestMethod.GET)
+	public String getAllClient(ModelMap model){
+		List<Client> liste = personneService.getAllClients();
+		model.addAttribute("listeclient", liste);
+		return null; //mettre une rediction
+	}
+	
+	/* CODER METHODE get CLIENT BY CONTROLLER QUAND SESSION VUE OK*/
+	
+	
+	
+}
